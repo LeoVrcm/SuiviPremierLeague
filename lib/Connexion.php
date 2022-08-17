031b17be91c703528d5cb8a1b2a6dcc1966521b5
LeoVrcm <111438170+LeoVrcm@users.noreply.github.com>
2022-08-17 16:38:43 +0200

    Add files via upload

--- lib/Connexion.php
+++ lib/Connexion.php
@@ -1,1 +1,41 @@
+<?php
 
+class Connexion {
+
+public function Connect($pathConnect) {
+
+    $tProperties = parse_ini_file($pathConnect);
+
+    $lsProtocol = $tProperties["protocol"];
+    $lsServer = $tProperties["server"];
+    $lsPort = $tProperties["port"];
+    $lsUS = $tProperties["us"];
+    $lsPWD = $tProperties["pwd"];
+    $lsDB = $tProperties["db"];
+    
+    $pdo = null;
+    
+    try {
+        
+        // Connexion selon 3 params: chaine de cnx, user et pwd
+        $pdo = new PDO("$lsProtocol:host=$lsServer;port=$lsPort;dbname=$lsDB;", $lsUS, $lsPWD);
+        // Les erreurs sont traitées comme des exceptions d'où le try/catch
+        $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // tuyau en UTF8, donc liason client-serveur
+        $pdo->exec("SET NAMES 'UTF8'");
+        
+    } catch (PDOException $e) {
+        echo "Execution failed: " . $e->getMessage();
+    }
+    
+    return $pdo;
+}
+
+public function Disconnect(PDO & $pcnx) {
+    $pcnx = null;
+}
+
+
+}
+
+?>
